e6463cf62b61a638ab1704793a77965188d1b03d
Bowluckie <bowluckie@gmail.com>
2026-05-31 21:12:45 +1000

    updated lualine.lua
    made lualine drop the *M and the :SS and added back the clock

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,1 +1,14 @@
-return { { "nvim-lualine/lualine.nvim", opts = { sections = { lualine_z = { function() return " " .. os.date("%r") end, }, }, }, }, } 
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      sections = {
+        lualine_z = {
+          function()
+            return " " .. os.date("%I:%M"):gsub("^0", "")
+          end,
+        },
+      },
+    },
+  },
+}
