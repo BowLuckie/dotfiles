5e498ce63fd170528449f5021bc32cc0c548a085
BowLuckie <bowluckie@gmail.com>
2026-06-15 21:28:28 +1000

    arch compat

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,10 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.shell = '"C:/Program Files/WindowsApps/Microsoft.PowerShell_7.6.2.0_x64__8wekyb3d8bbwe/pwsh.exe"'
-vim.opt.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command"
-vim.opt.shellquote = ""
-vim.opt.shellxquote = ""
+vim.opt.clipboard = "unnamedplus"
 
 vim.opt.termguicolors = true
 vim.opt.number = true
