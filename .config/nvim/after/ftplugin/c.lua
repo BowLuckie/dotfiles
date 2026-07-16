f954709eae16f7110d0d0be2efa8b138c97b4460
BowLuckie <bowluckie@gmail.com>
2026-07-16 21:28:33 +1000

    added wallpapers

--- .config/nvim/after/ftplugin/c.lua
+++ .config/nvim/after/ftplugin/c.lua
@@ -1,4 +1,19 @@
-vim.opt_local.tabstop = 2
-vim.opt_local.shiftwidth = 2
-vim.opt_local.softtabstop = 2
+local function get_clang_format_indent()
+  local clang_format_path = vim.fn.findfile(".clang-format", ".;")
+  if clang_format_path == "" then
+    return nil
+  end
+  for line in io.lines(clang_format_path) do
+    local width = line:match("IndentWidth:%s*(%d+)")
+    if width then
+      return tonumber(width)
+    end
+  end
+  return nil
+end
+
+local indent = get_clang_format_indent() or 4
+vim.opt_local.tabstop = indent
+vim.opt_local.shiftwidth = indent
+vim.opt_local.softtabstop = indent
 vim.opt_local.expandtab = true
